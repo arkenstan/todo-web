d3e31c14f7ddfeff04162039864a178580093499
arkenstan <anshulwagadre@gmail.com>
2019-11-06 09:30:38 +0530

    updated tests

--- src/app/task/components/task-item/task-item.component.spec.ts
+++ src/app/task/components/task-item/task-item.component.spec.ts
@@ -3,6 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SharedModule } from '@shared/shared.module';
+import { Task } from '@app/task/models/task.model';
 import { TaskMaterial } from '@shared/material/material.module';
 import { TaskItemComponent } from './task-item.component';
 
@@ -20,6 +21,16 @@ describe('TaskItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TaskItemComponent);
     component = fixture.componentInstance;
+    let expectedTask: Task = {
+      _id: 'task_id',
+      refId: 'task_ref_id',
+      completed: false,
+      content: 'Task 1 Title',
+      createdBy: 'user_id_slug',
+      createdAt: new Date(),
+      updateAt: new Date()
+    };
+    component.task = expectedTask;
     fixture.detectChanges();
   });
 
